ae196792c8ba9f1c18e7b8fd9641554e0d4e85bc
MelaniaPicone <melania_picone@yahoo.it>
2025-07-02 20:36:31 +0200

    [corr] for

--- js/script.js
+++ js/script.js
@@ -3,22 +3,22 @@
 console.log("Colonna numerica");
 const i=0;
 for(let i = 1; i<=100; i++) {
-  console.log(i);
-}
 
 // stampa per i multipli di 3 e 5
 
 if(i % 3 === 0) {
-console.log("Fizz");
-}
+console.log("Fizz")}
 
 else if(i % 5 === 0) {
-
+console.log("Buzz")
 }
 
 else if (i % 3 === 0 && i % 5 === 0) {
+console.log("FizzBuzz")
 }
 
 else {
-  
+console.log(i)
+}
+
 }
